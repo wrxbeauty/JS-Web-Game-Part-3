b41bed350600ab0c9a3d7b8c34c7364ecaabaea7
Casey Halbohn <wrxbeauty15@hotmail.com>
2022-12-18 13:10:56 -0500

    updated

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 function newImage(url, left, bottom){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
+// newImage('assets/green-character.gif', 100, 250)
+// newImage('assets/tree.png', 200, 450)
+// newImage('assets/pillar.png', 350, 250)
+// newImage('assets/pine-tree.png', 450, 350)
+// newImage('assets/crate.png', 150, 350)
+// newImage('assets/well.png', 500, 575)
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
@@ -27,9 +23,9 @@ function newItem(url, left, bottom){
     return item
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+// newItem('assets/sword.png', 500, 555)
+// newItem('assets/shield.png', 165, 335)
+// newItem('assets/staff.png', 600, 250)
 
 function newInventory(){
     let inventory = document.createElement('div')
@@ -48,4 +44,37 @@ function newInventory(){
     return inventory
 }
 
+function move(image){
+    image.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
+}
+
+// let thingThatMoveReturns = move(greenCharacter)
+// thingThatMoveReturns.to
+// thingThatMoveReturns.to(300, 300)
+
+// let greenCharacter = newImage('assets/green-character.gif')
+// let thingThatMoveReturns = move(greenCharacter)
+// thingThatMoveReturns.to(100, 250)
+
 const inventory = newInventory()
+
+move(inventory).to(0, 0)
+move(newImage('assets/green-character.gif')).to(100, 250)
+move(newImage('assets/green-character.gif')).to(100, 250)
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
